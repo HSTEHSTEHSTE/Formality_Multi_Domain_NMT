b971869e9bf1d7fa578774f436b7f3e853c4637e
Henry Li <xli257@jhu.edu>
2021-11-10 14:41:36 -0500

    Add positional encoder

--- src/model.py
+++ src/model.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
+import math
 
 class TransformerEncoder(nn.Module):
     """Transformer encoder"""
@@ -12,7 +13,7 @@ class TransformerEncoder(nn.Module):
         self.dropout = dropout
         self.embedder = nn.Embedding(num_embeddings=self.input_size, embedding_dim=self.d_model)
 
-        self.transformer_encoder_layer = nn.TransformerEncoderLayer(d_model=self.d_model)
+        self.transformer_encoder_layer = nn.TransformerEncoderLayer(d_model=self.d_model, dropout=self.dropout)
         self.transformer_encoder = nn.TransformerEncoder(self.transformer_encoder_layer, num_layers=self.nlayers)
 
     def forward(self, input):
@@ -27,14 +28,60 @@ class TransformerDecoder(nn.Module):
         self.target_size = target_size
         self.d_model = d_model
         self.n_layers = n_layers
+        self.dropout = dropout
         self.embedder = nn.Embedding(num_embeddings=self.target_size, embedding_dim=self.d_model)
+        self.position_encoder = PositionalEncoding(d_model=self.d_model, dropout=self.dropout)
         self.decoder = nn.Linear(in_features=self.d_model, out_features=self.target_size)
 
-        self.transformer_decoder_layer = nn.TransformerDecoderLayer(d_model=self.d_model)
+        self.transformer_decoder_layer = nn.TransformerDecoderLayer(d_model=self.d_model, dropout=self.dropout)
         self.transformer_decoder = nn.TransformerDecoder(self.transformer_decoder_layer, num_layers=self.n_layers)
 
     def forward(self, target, memory):
         target_embedded = self.embedder(target)
+        target_embedded = self.position_encoder(target_embedded)
         output_embedded = self.transformer_decoder(target_embedded, memory)
         output = self.decoder(output_embedded)
         return output
+
+class PositionalEncoding(nn.Module):
+    r"""Inject some information about the relative or absolute position of the tokens
+        in the sequence. The positional encodings have the same dimension as
+        the embeddings, so that the two can be summed. Here, we use sine and cosine
+        functions of different frequencies.
+    .. math::
+        \text{PosEncoder}(pos, 2i) = sin(pos/10000^(2i/d_model))
+        \text{PosEncoder}(pos, 2i+1) = cos(pos/10000^(2i/d_model))
+        \text{where pos is the word position and i is the embed idx)
+    Args:
+        d_model: the embed dim (required).
+        dropout: the dropout value (default=0.1).
+        max_len: the max. length of the incoming sequence (default=5000).
+    Examples:
+        >>> pos_encoder = PositionalEncoding(d_model)
+    """
+
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        r"""Inputs of forward function
+        Args:
+            x: the sequence fed to the positional encoder model (required).
+        Shape:
+            x: [sequence length, batch size, embed dim]
+            output: [sequence length, batch size, embed dim]
+        Examples:
+            >>> output = pos_encoder(x)
+        """
+
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
