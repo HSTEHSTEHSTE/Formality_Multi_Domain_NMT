0127e25833410e98239b574af8d0f7c2dff87d92
Henry Li <xli257@jhu.edu>
2021-11-12 12:14:45 -0500

    Add sentence vec similarity implementation

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,4 +1,7 @@
 import nltk
+import os
+import numpy as np
+import tqdm
 
 def calculate_bleu_score(hypothesis, reference):
     """
@@ -23,3 +26,32 @@ def calculate_corpus_bleu_score(hypotheses, references):
     """
     score = nltk.translate.bleu_score.sentence_bleu(references, hypotheses)
     return score
+
+def load_word_to_vec_file():
+    word_to_vec_file = open(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), "data/cc.ja.300.vec"), "r", encoding="utf-8")
+    header = word_to_vec_file.readline()
+    header_list = header.split()
+    vocab_size = int(header_list[0])
+    embedding_dim = int(header_list[1])
+    embedding_dict = {}
+    for line in tqdm.tqdm(word_to_vec_file, total=vocab_size):
+        line_list = line.split()
+        word = line_list[0]
+        embedding_vector = np.array([float(x) for x in line_list[1:]])
+        embedding_dict[word] = embedding_vector
+    return embedding_dict, embedding_dim, vocab_size
+
+def calculate_sentence_similarity(hypothesis, reference, embedding_dict, embedding_dim):
+    """
+    Input:  hypotheses: list(str)
+            references: list(str)
+    Output: similarity: float
+    """
+    hypothesis_embedding = np.zeros(embedding_dim)
+    reference_embedding = np.zeros(embedding_dim)
+    for hypothesis_word in hypothesis:
+        hypothesis_embedding += embedding_dict[hypothesis_word]
+    for reference_word in reference:
+        reference_embedding += embedding_dict[reference_word]
+    similarity = np.dot(hypothesis_embedding, reference_embedding)/(np.linalg.norm(hypothesis_embedding) * np.linalg.nomr(reference_embedding))
+    return similarity
