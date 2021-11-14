66694f09c6e7b94a7d07f66f38e022c476cbce3b
Henry Li <xli257@jhu.edu>
2021-11-13 22:21:55 -0500

    More robust scheduling

--- src/pre-trained.py
+++ src/pre-trained.py
@@ -10,11 +10,12 @@ tokenizer = AutoTokenizer.from_pretrained("cl-tohoku/bert-base-japanese-char")
 
 # Hyper parameters
 batch_size = 128
-dev_batch_size = 32
+dev_batch_size = 128
 max_iterations = 100
 initial_learning_rate = .001
 lr_decay = .5
 print_every = 2
+lr_threshold = .0000001
 
 # Load data
 data_array = pd.read_csv(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), "data/data.csv"), header=None, index_col=None, delimiter=',')
@@ -36,6 +37,10 @@ lr = initial_learning_rate
 
 accuracies = []
 losses = []
+total_accuracy = 0
+total_loss = 0
+total_dev_accuracy = 0
+total_dev_loss = 0
 for iteration_number in range(0, max_iterations):
     batch_array = train_data_array.sample(n=batch_size)
     output_pooled_list = []
@@ -60,35 +65,47 @@ for iteration_number in range(0, max_iterations):
     # calculate training accuracy
     output_labels = torch.argmax(output_labels, dim=1)
     accuracy_tensor = torch.where(output_labels - target_labels < .5, torch.tensor(1), torch.tensor(0))
-    accuracy = torch.sum(accuracy_tensor) / batch_size
+    accuracy_batch = torch.sum(accuracy_tensor).item()
+    accuracy = accuracy_batch / batch_size
+    total_accuracy += accuracy
     accuracies.append(accuracy)
     losses.append(loss.item())
+    total_loss += loss.item()
+
+    # load dev data
+    dev_batch_array = dev_data_array.sample(n=dev_batch_size)
+    dev_output_pooled_list = []
+    for sentence_label_pair in tqdm.tqdm(dev_batch_array.iterrows(), total=dev_batch_size):
+        sentence = sentence_label_pair[1].iloc[0][:510] # magic number 512: pre-trained BERT length limit
+        inputs = tokenizer(sentence, return_tensors="pt")
+        output_pooled = bertjapanese(**inputs).pooler_output # shape [1, 768]
+        dev_output_pooled_list.append(output_pooled)
+    dev_target_labels = torch.tensor(dev_batch_array.iloc[:, [1]].to_numpy()).squeeze(1) # shape [dev_batch_size]
+
+    dev_classifier_input_batched = torch.stack(dev_output_pooled_list, dim=0) # shape [dev_batch_size, 768]
+    classifier.eval()
+
+    optimiser.zero_grad()
+    dev_output_labels = classifier(dev_classifier_input_batched).squeeze(1) # shape [dev_batch_size, 2]
+    dev_loss = criterion(dev_output_labels, dev_target_labels).item()
+    total_dev_loss += dev_loss
+
+    # calculate training accuracy
+    dev_output_labels = torch.argmax(dev_output_labels, dim=1)
+    dev_accuracy_tensor = torch.where(dev_output_labels - dev_target_labels < .5, torch.tensor(1), torch.tensor(0))
+    dev_accuracy = torch.sum(dev_accuracy_tensor).item()
+    total_dev_accuracy += dev_accuracy
 
     # calculate dev loss, update learning rate
     if (iteration_number + 1) % print_every == 0:
-        print("Iteration ", iteration_number + 1, " , loss is ", loss.item(), " , training accuracy is ", accuracy.item())
-
-        dev_batch_array = dev_data_array.sample(n=dev_batch_size)
-        dev_output_pooled_list = []
-        for sentence_label_pair in tqdm.tqdm(dev_batch_array.iterrows(), total=dev_batch_size):
-            sentence = sentence_label_pair[1].iloc[0][:510] # magic number 512: pre-trained BERT length limit
-            inputs = tokenizer(sentence, return_tensors="pt")
-            output_pooled = bertjapanese(**inputs).pooler_output # shape [1, 768]
-            dev_output_pooled_list.append(output_pooled)
-        dev_target_labels = torch.tensor(dev_batch_array.iloc[:, [1]].to_numpy()).squeeze(1) # shape [dev_batch_size]
-
-        dev_classifier_input_batched = torch.stack(dev_output_pooled_list, dim=0) # shape [dev_batch_size, 768]
-        classifier.eval()
-
-        optimiser.zero_grad()
-        dev_output_labels = classifier(dev_classifier_input_batched).squeeze(1) # shape [dev_batch_size, 2]
-        dev_loss = criterion(dev_output_labels, dev_target_labels)
-
-        # calculate training accuracy
-        dev_output_labels = torch.argmax(dev_output_labels, dim=1)
-        dev_accuracy_tensor = torch.where(dev_output_labels - dev_target_labels < .5, torch.tensor(1), torch.tensor(0))
-        dev_accuracy = torch.sum(dev_accuracy_tensor) / dev_batch_size
-        print("Dev loss is ", dev_loss.item(), " , dev accuracy is ", dev_accuracy.item())
+        print("Iteration ", iteration_number + 1, " , loss is ", total_loss / print_every, " , training accuracy is ", total_accuracy / print_every)
+        total_loss = 0
+        total_accuracy = 0
+        
+        dev_loss = total_dev_loss / print_every
+        print("Dev loss is ", dev_loss, " , dev accuracy is ", total_dev_accuracy / (batch_size * print_every))
+        total_dev_loss = 0
+        total_dev_accuracy = 0
         
         # update learning rate
         if previous_loss is not None and previous_loss < dev_loss:
@@ -97,5 +114,7 @@ for iteration_number in range(0, max_iterations):
             lr = lr_new
             for param_group in optimiser.param_groups:
                 param_group["lr"] = lr
-        else:
-            previous_loss = dev_loss
+        previous_loss = dev_loss
+
+        if lr < lr_threshold:
+            break
