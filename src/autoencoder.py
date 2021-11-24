bce5e6c576713548a740c5095623971e5469540c
Henry Li <xli257@jhu.edu>
2021-11-24 15:15:30 -0500

    Add forward pass to autoencoder.py

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -39,11 +39,11 @@ ja_dictionary_size = len(ja_dict)
 ja_embedding = nn.Embedding(ja_dictionary_size, 512, padding_idx=1) # 512: default embed_dim; 1: default pad value
 
 # Build encoder and decoder objects
-encoder = TransformerEncoder(config, ja_dict, ja_embedding)
-decoder = TransformerDecoder(config, ja_dict, ja_embedding)
+encoder = TransformerEncoder(config, ja_dict, ja_embedding).to(device=device)
+decoder = TransformerDecoder(config, ja_dict, ja_embedding).to(device=device)
 
 # Load data
-data_array = pd.read_csv(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), "data/combined_with_label.txt"), header=None, index_col=None, delimiter='\\|\\|')
+data_array = pd.read_csv(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), "data/combined_with_label.txt"), header=None, index_col=None, delimiter='\\|\\|').dropna()
 data_size = data_array.shape[0]
 dev_size = int(.1 * data_size)
 test_size = int(.1 * data_size)
@@ -60,16 +60,22 @@ for iteration_number in range(0, max_iterations):
     batch_array = train_data_array.sample(n=batch_size)
 
     sentence_tensors = []
+    sentence_lengths = []
     for sentence_label_pair in tqdm.tqdm(batch_array.iterrows(), total=batch_size):
         # [JA], [EN], label
         sentence = sentence_label_pair[1].iloc[0]
         sentence_tensor = ja_dict.encode_line(sentence) # (len(sentence) + 1)
-        sentence_tensors.append(F.pad(sentence_tensor, (0, 0, 0, max_sentence_length - sentence_tensor.shape[0]), value = ja_dict.pad()))[:max_sentence_length] # (max_sentence_length)
+        sentence_lengths.append(min(max_sentence_length, sentence_tensor.shape[0]))
+        sentence_tensors.append(F.pad(sentence_tensor, (0, max_sentence_length - sentence_tensor.shape[0]), value = ja_dict.pad())[:max_sentence_length]) # (max_sentence_length)
 
     sentence_tensors = torch.stack(sentence_tensors, dim=0).to(device=device) # (batch_size, max_sentence_length)
+    sentence_lengths = torch.tensor(sentence_lengths).to(device=device) # (batch_size)
 
     encoder.train()
     decoder.train()
     optimiser.zero_grad()
 
     # main forward pass
+    encoder_dict = encoder(sentence_tensors, sentence_lengths)
+    decoder_output = decoder(sentence_tensors, encoder_dict)[0]
+
