b3994895cf057904fafc6c2450f06ce605aaeb33
Henry Li <xli257@jhu.edu>
2021-11-24 14:43:23 -0500

    Add batch data loading

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+import torch.nn.functional as F
 from fairseq.models.transformer.transformer_encoder import TransformerEncoder
 from fairseq.models.transformer.transformer_decoder import TransformerDecoder
 from fairseq.models.transformer.transformer_config import TransformerConfig
@@ -7,6 +8,14 @@ from fairseq.data.dictionary import Dictionary
 import os
 import tqdm
 import pandas as pd
+
+# Hyper parameters
+batch_size = 128
+max_iterations = 100
+initial_learning_rate = .001
+max_sentence_length = 20
+use_gpu = True
+device = torch.device("cuda:0" if (torch.cuda.is_available() and use_gpu) else "cpu")
 
 # Build config objects
 config = TransformerConfig() # default config
@@ -33,6 +42,7 @@ ja_embedding = nn.Embedding(ja_dictionary_size, 512, padding_idx=1) # 512: defau
 encoder = TransformerEncoder(config, ja_dict, ja_embedding)
 decoder = TransformerDecoder(config, ja_dict, ja_embedding)
 
+# Load data
 data_array = pd.read_csv(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), "data/combined_with_label.txt"), header=None, index_col=None, delimiter='\\|\\|')
 data_size = data_array.shape[0]
 dev_size = int(.1 * data_size)
@@ -41,3 +51,25 @@ data_array = data_array.sample(frac=1)
 dev_data_array = data_array.loc[:dev_size]
 test_data_array = data_array.loc[dev_size:dev_size + test_size]
 train_data_array = data_array.loc[dev_size + test_size:]
+
+# Build criterion and optimiser
+criterion = torch.nn.NLLLoss()
+optimiser = torch.optim.Adam(list(encoder.parameters()) + list(decoder.parameters()), lr=initial_learning_rate, weight_decay=0)
+
+for iteration_number in range(0, max_iterations):
+    batch_array = train_data_array.sample(n=batch_size)
+
+    sentence_tensors = []
+    for sentence_label_pair in tqdm.tqdm(batch_array.iterrows(), total=batch_size):
+        # [JA], [EN], label
+        sentence = sentence_label_pair[1].iloc[0]
+        sentence_tensor = ja_dict.encode_line(sentence) # (len(sentence) + 1)
+        sentence_tensors.append(F.pad(sentence_tensor, (0, 0, 0, max_sentence_length - sentence_tensor.shape[0]), value = ja_dict.pad()))[:max_sentence_length] # (max_sentence_length)
+
+    sentence_tensors = torch.stack(sentence_tensors, dim=0).to(device=device) # (batch_size, max_sentence_length)
+
+    encoder.train()
+    decoder.train()
+    optimiser.zero_grad()
+
+    # main forward pass
